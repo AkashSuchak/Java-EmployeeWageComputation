783a93f4c652d4542bd43e3ec09833d89b7d545c
AkashSuchak <suchak.akash@gmail.com>
2020-10-27 22:45:44 +0530

    Added : PartTime Employee And Wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,5 +1,5 @@
 //Author :  Akash Suchak
-//Computing Day Wage Of Employee
+//Computing Day Wage Of Employee(Fulltime And PartTime)
 
 //package
 import java.util.Random;
@@ -9,25 +9,36 @@ public class EmpWageComp {
         //object of class
         EmpWageComp ewc = new EmpWageComp();
 
-        //To Generate Randoms 0 or 1
+        //To Generate Randoms 0, 1 and 2
         Random ran= new Random();
-        int ran1 = ran.nextInt(2);
+        int ran1 = ran.nextInt(3);
 
         //Employee is Present or Not
         if( ran1 == 0 ) {
-            System.out.println("Employee is Present");
-            ewc.wage();
+            System.out.println("Employee is Present and Working FullTime");
+            ewc.fullTime();
+        }else if (ran1 == 1){
+            System.out.println("Employee is Present and Working PartTime");
+            ewc.partTime();
         }else {
             System.out.println("Employee is Absent");
         }
+
     }
-    //Method to count Day Wage
-    public void wage(){
+    //Method to count Fulltime Day Wage
+    public void fullTime(){
         int wagePerHr = 20;
         int dailyWorkHrs = 8;
 
         int totalDayWage = wagePerHr * dailyWorkHrs;
         System.out.println("Wage : " + totalDayWage);
     }
-}
+    //Method to count Parttime Day Wage
+    public void partTime(){
+        int wagePerHr = 20;
+        int dailyWorkHrs = 4;
 
+        int totalDayWage = wagePerHr * dailyWorkHrs;
+        System.out.println("Wage : " + totalDayWage);
+    }
+}
