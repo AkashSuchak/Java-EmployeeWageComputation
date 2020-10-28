e52638378b1caa3e832b0976922b4d3ca31a9acb
AkashSuchak <suchak.akash@gmail.com>
2020-10-29 02:04:29 +0530

    Refactor Code : Using Fuction to Compute Monthly Wage

--- EmpWageComp.java
+++ EmpWageComp.java
@@ -1,40 +1,46 @@
 //Author :  Akash Suchak
-//Calculate Monthly Wage Untill TotalWorkingHours Or Days is reached
+//Refactor the code to write a class Method to compute Employee Wage
 
 //package
 import java.util.Random;
 
 public class EmpWageComp {
-    public static void main(String[] args){
-        //Assign Values for wage
-        int wagePerHour=20;
-        int fullDayHours=8;
-        int halfDayHours=4;
-        int workingDays=20;
-        int totalWorkingHours=0;
+    //Variables To Count Month Wages
+    static int wagePerHour = 20, day = 1;
+    static int fullDayHours = 8, halfDayHours = 4, totalWorkingHours = 0;
 
-        for (int i=0; i<workingDays; i++) {
-            //To Generate Randoms 0, 1 and 2
-            Random ran = new Random();
-            int ran1 = ran.nextInt(3);
-
-            //Employee is Present or Not
-            if (ran1 == 0) {
-                totalWorkingHours += fullDayHours;
-            } else if (ran1 == 1) {
-                totalWorkingHours += halfDayHours;
-            } else {
-                totalWorkingHours += 0;
-            }
-            //Setting Working Hours 100 if greater than 100
-            if (totalWorkingHours > 100) {
-                totalWorkingHours = 100;
-                break;
-            }
+    public static void main(String[] args) {
+        EmpWageComp emp = new EmpWageComp();
+        emp.days();
+        //Setting Working Hours 100 if greater 100
+        if (totalWorkingHours > 100) {
+            totalWorkingHours = 100;
         }
-        //Display Total Working Hours with Total Wage
-        int totalMonthlyWage=totalWorkingHours * wagePerHour;
-        System.out.println("Total Working Hours : " + totalWorkingHours );
+        emp.totalWage();
+    }
+
+    //Method of Display Total Working Hours with Total Wage
+    public void totalWage() {
+        int totalMonthlyWage = totalWorkingHours * wagePerHour;
+        System.out.println("Total Working Hours : " + totalWorkingHours);
         System.out.println("Total Wage : " + totalMonthlyWage);
+    }
+    //Method to count 20 days
+    public void days() {
+        //To Generate Randoms 0, 1 and 2
+        Random ran = new Random();
+        int ran1 = ran.nextInt(3);
+
+        //Employee is Present or Not
+        if (ran1 == 0) {
+            totalWorkingHours += fullDayHours;
+        } else if (ran1 == 1) {
+            totalWorkingHours += halfDayHours;
+        }
+        day += 1;
+        if (day <= 20) {
+            EmpWageComp emp = new EmpWageComp();
+            emp.days();
+        }
     }
 }
